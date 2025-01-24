a5497c816b87797c3df1b81643317c9c5ee281cc
Waliullah Muntasir <waliullah.muntasir@gmail.com>
2025-01-24 22:27:27 +0600

    whu

--- js.js
+++ js.js
@@ -1,4 +1,11 @@
-var make = document.getElementById('ggg');
-make.addEventListener('click',function wehew(){
-    document.body.style.background = 'green'
-})
+
+function gta() {
+    var dvcgbye = document.getElementById('textareaaaa');
+    var wheufgvrsuwer = dvcgbye.value;
+    var yyyyy = document.getElementById('world-cup');
+    var p = document.createElement('p');
+    p.innerText = wheufgvrsuwer;
+    yyyyy.appendChild(p);
+    console.log(p);   
+    
+}
